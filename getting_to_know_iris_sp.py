3f7c1f7fc80a67af486581c8f0ae7f3148090e7a
Ellyanne24 <154413656+Ellyanne24@users.noreply.github.com>
2023-12-21 15:38:30 +0800

    Update getting_to_know_iris_sp.py

--- getting_to_know_iris_sp.py
+++ getting_to_know_iris_sp.py
@@ -11,9 +11,9 @@ st.sidebar.header('Score the following characteristics')
 
 def user_input_features():
     sepal_length = st.sidebar.slider('Sepal Length', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('Sepal Width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal Length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('Petal Width', 0.1, 2.5, 0.2)
+    sepal_width = st.sidebar.slider('Sepal Width', 2.0, 4.4, 3.1)
+    petal_length = st.sidebar.slider('Petal Length', 1.0, 6.9, 3.9)
+    petal_width = st.sidebar.slider('Petal Width', 0.1, 2.5, 1.5)
     data = {'sepal_length': sepal_length,
             'sepal_width': sepal_width,
             'petal_length': petal_length,
